ae4b45b87176a272b88a5ab104b1c7e48ca12dde
linkxs <admin@linkxs.org>
2013-06-05 17:17:28 -0700

    Fixed includes

--- src/main.java
+++ src/main.java
@@ -1,3 +1,5 @@
+import graph.DfsGraph;
+
 // Project 4: Strongly Connected Components - Due Wed June 5
 //
 // Develop a program to compute the strongly connected components of a directed graph.
@@ -9,6 +11,7 @@
 // 2. Use the DfsGraph class provided and depth-first search to implement the strongly connected components algorithm 
 //  described by pseudocode on the course site and run it on the initial test graphs.
 //
+//
 // 3. Implement your own version of the DfsGraph class and make sure it produces correct results.
 //
 // 4. Run your program on the final test graph.
@@ -17,33 +20,22 @@
 //
 
 public class main {
+	static boolean	DEBUG	= true;
+	
 	public static void main(String[] args) {
-		// Scanner s = new Scanner(System.in);
-		// int order = s.nextInt(), size = s.nextInt();
-		// boolean directed = false, weighted = true;
-		//
-		// if (s.next().equalsIgnoreCase("directed")) {
-		// directed = true;
-		// weighted = false;
-		// }
-		// DfsGraph g = new DfsGraph("Graph", order, size, directed, weighted);
-		//
-		// while (s.hasNext()) {
-		// if (weighted)
-		// g.addEdge(s.nextInt(), s.nextInt(), s.nextDouble());
-		// else
-		// g.addEdge(s.nextInt(), s.nextInt());
-		// }
-		//
-		
 		DfsGraph g = null;
 		try {
-			g = new DfsGraph(args[0]);
+			if (!DEBUG)
+				g = new DfsGraph(args[0]);
+			else
+				g = new DfsGraph("~/Downloads/graph3.txt");
 		} catch (Exception e) {
 			System.out.println("Wrong arguments. Expected: filename");
 			System.exit(1);
 		}
 		System.out.println(g.toString());
-		g.dfs(0);
+		// g.dfs(0);
+		// for (int i = 0; i < g.getOrder(); i++)
+		// System.out.println(g.getVertex(i).getIndex());
 	}
 }
